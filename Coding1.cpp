72612425efffd197fa866f2947b749444123fcc9
dhanendraverma <46896989+dhanendraverma@users.noreply.github.com>
2019-06-08 12:51:57 +0530

    Solution of 1st coding question

---
+++ Coding1.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************************
+
+Given a string  A, consisting of only lowercase english characters. Complete the function
+which returns a string, containing each letter in the string snd it's occurance in the
+same order as they are in the given string. No letter should be reapeted in the 
+retunrned string.
+
+******************************************************************************************/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string countoccurance(string A)
+{
+    string ans = "";
+    int count[26] = {};
+    for(int i=0;i<A.length();i++)
+        count[A[i]-'a']++;
+    
+    for(int i=0;i<A.length();i++)
+    {
+        if(count[A[i]-'a']>0)
+        {
+            ans += A[i];
+            ans += to_string(count[A[i]-'a']);
+            count[A[i]-'a'] = 0;
+            
+        }
+    }
+    
+    return ans;
+}
+
+
+
+int main()
+{
+    string A;
+    cin>>A;
+    cout<<countoccurance(A);
+
+    return 0;
+}
